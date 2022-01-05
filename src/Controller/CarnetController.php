930d027250bd6a2aa4db7185b6897e51cad2edfb
Arno RAMM <Arno.Ramm@telecomnancy.eu>
2022-01-05 12:11:57 +0100

    visualisation fiche client

---
+++ src/Controller/CarnetController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Client;
+use App\Form\ClientFormType;
+use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CarnetController extends AbstractController
+{
+    /**
+     * @Route("/carnet/ajout", name="carnet_ajout")
+     */
+    public function index(Request $request, EntityManagerInterface $manager): Response
+    {
+
+        $client = new Client();
+
+        $form = $this->createForm(ClientFormType::class, $client);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+
+            $client->setCorrespondingUser($this->getUser());
+
+            $manager->persist($client);
+            $manager->flush();
+
+
+            return $this->render('home/home.twig');
+        }
+
+
+        return $this->render('carnet_adresse/ajout.twig', [
+            "clientForm" => $form->createView()
+        ]);
+    }
+
+    // route permettant de visualiser une fiche client
+    /**
+     * @Route("/carnet/client/{id}", name="carnet_view")
+     */
+    public function voir_client(int $id, Request $request, EntityManagerInterface $manager): Response
+    {
+        // récupération de l'objet repository permettant d'effectuer les requêtes
+        $repository = $manager->getRepository(Client::class);
+
+        // recherche par id (paramètre de la route)
+        $client = $repository->findBy(array('id' => $id))[0];
+
+        return $this->render('carnet_adresse/client.twig', [
+            'client' => $client
+        ]);
+    }
+
+}
---
+++ src/Entity/Client.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ClientRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ClientRepository::class)
+ */
+class Client
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $firstName;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $lastName;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $societe;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $telephone;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\User")
+     */
+    private $corresponding_user;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(string $firstName): self
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName(string $lastName): self
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getSociete(): ?string
+    {
+        return $this->societe;
+    }
+
+    public function setSociete(string $societe): self
+    {
+        $this->societe = $societe;
+
+        return $this;
+    }
+
+    public function getTelephone(): ?string
+    {
+        return $this->telephone;
+    }
+
+    public function setTelephone(string $telephone): self
+    {
+        $this->telephone = $telephone;
+
+        return $this;
+    }
+
+    public function getCorrespondingUser(): ?User
+    {
+        return $this->corresponding_user;
+    }
+
+    public function setCorrespondingUser(User $corresponding_user): self
+    {
+        $this->corresponding_user = $corresponding_user;
+
+        return $this;
+    }
+}

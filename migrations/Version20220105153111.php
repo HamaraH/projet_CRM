27a05821b64369ffe25785732b7310a526e39cb0
Manon5 <manon.touba05@gmail.com>
2022-01-05 16:55:16 +0100

    style sur les taches

--- migrations/Version20220105153111.php
+++ migrations/Version20220105153111.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220105142942 extends AbstractMigration
+final class Version20220105153111 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,17 +20,17 @@ final class Version20220105142942 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE tache ADD corresponding_client_id INT DEFAULT NULL');
-        $this->addSql('ALTER TABLE tache ADD CONSTRAINT FK_938720754DF89C0E FOREIGN KEY (corresponding_client_id) REFERENCES client (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('CREATE SEQUENCE tache_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE TABLE tache (id INT NOT NULL, corresponding_client_id INT DEFAULT NULL, content VARCHAR(1000) NOT NULL, publication_date TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, titre VARCHAR(255) NOT NULL, etat VARCHAR(40) NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX IDX_938720754DF89C0E ON tache (corresponding_client_id)');
+        $this->addSql('ALTER TABLE tache ADD CONSTRAINT FK_938720754DF89C0E FOREIGN KEY (corresponding_client_id) REFERENCES client (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
-        $this->addSql('ALTER TABLE tache DROP CONSTRAINT FK_938720754DF89C0E');
-        $this->addSql('DROP INDEX IDX_938720754DF89C0E');
-        $this->addSql('ALTER TABLE tache DROP corresponding_client_id');
+        $this->addSql('DROP SEQUENCE tache_id_seq CASCADE');
+        $this->addSql('DROP TABLE tache');
     }
 }
